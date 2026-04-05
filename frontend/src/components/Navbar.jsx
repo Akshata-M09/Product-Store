15fdf86daf28d879dfb405822e5db3440450df51
Akshata-M09 <89059087+Akshata-M09@users.noreply.github.com>
2026-04-05 19:08:50 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           bgGradient={"linear(to-r, cyan.400,blue.500)"}
           bgClip={"text"}
         >
-          <Link to={"/"}>Product Store 🛒</Link>
+          <Link to={"/"}>Product Storeeeeeeeee 🛒</Link>
         </Text>
         <HStack spacing={2} alignItems={"center"}>
           <Link to={"/create"}>
